84743458d07e652d6fb5ab313aea4b64da13e2e7
migueldlozano <miguel.d.lozano@gmail.com>
2016-12-06 13:02:37 -0500

    Add Store Action/Dispatch support

--- app/redux-examples.jsx
+++ app/redux-examples.jsx
@@ -2,64 +2,73 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-// PureFunctions
-// 01 // input arguments do not get mutated
-// 02 // only local vars, no outside vars
-// 03 // AVOID  promises and async calls, like db calls or http requests
-
-
-// pureFunction
-function add(a, b) {
-  return a + b;
-}
-
-// notPure
-var a = 3
-
-function sum(b) {
-  return a + b;
-}
-
-// alsoNotPure
-var result;
-function add(a, b) {
-  result = a + b;
-  return result;
-}
-
-// suchNotPure
-function add(a, b) {
-  return a + b + new Date().getSeconds();
-}
-
-// pureFunctions cannot mutate arguments
-
-
-function changeProp(obj) {
-  // // PassingByReference
-  // obj.name = 'Jen';
-  // return obj
-
-  // PassingByValue
-   return {
-     ...obj,
-     name: 'Jen'
-   }
-}
-
-var startingValue = {
-  name: 'Miguel',
-  age: 27
-}
-
-var res = changeProp(startingValue);
-console.log(startingValue);
-console.log(res);
+// // PureFunctions
+// // 01 // input arguments do not get mutated
+// // 02 // only local vars, no outside vars
+// // 03 // AVOID  promises and async calls, like db calls or http requests
+//
+//
+// // pureFunction
+// function add(a, b) {
+//   return a + b;
+// }
+//
+// // notPure
+// var a = 3
+//
+// function sum(b) {
+//   return a + b;
+// }
+//
+// // alsoNotPure
+// var result;
+// function add(a, b) {
+//   result = a + b;
+//   return result;
+// }
+//
+// // suchNotPure
+// function add(a, b) {
+//   return a + b + new Date().getSeconds();
+// }
+//
+// // pureFunctions cannot mutate arguments
+//
+//
+// function changeProp(obj) {
+//   // // PassingByReference
+//   // obj.name = 'Jen';
+//   // return obj
+//
+//   // PassingByValue
+//    return {
+//      ...obj,
+//      name: 'Jen'
+//    }
+// }
+//
+// var startingValue = {
+//   name: 'Miguel',
+//   age: 27
+// }
+//
+// var res = changeProp(startingValue);
+// console.log(startingValue);
+// console.log(res);
 
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'};
-  return state;
+  
+  switch(action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      default:
+        return state;
+  }
 }
 
 var store = redux.createStore(reducer);
@@ -67,3 +76,11 @@ var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Miguel',
+});
+
+console.log('Name should be Miguel', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,10 +9,32 @@ var stateDefault = {  searchText: '',
 
 var reducer = (state = stateDefault, action) => {
   return state;
+
 }
+
+var reducer = (state = stateDefault, action) => {
+  // state = state || {name: 'Anonymous'};
+
+  switch(action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+      default:
+        return state;
+  }
+}
+
 
 var store = redux.createStore(reducer);
 
-
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'work',
+});
+
+console.log('searchText should be \'work\'', store.getState());
